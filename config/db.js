59e26b8bcbc6570772ee5c9ce5b456611f5fa646
blvckhaul1 <victorotuoma5@gmail.com>
2025-08-11 07:38:42 +0300

    Update Db Connection Limit

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 3,
+  connectionLimit: 5,
   queueLimit: 0,
 });
 
